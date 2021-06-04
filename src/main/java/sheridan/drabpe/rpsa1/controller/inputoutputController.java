7f1e92f826f75bde6f4cf2cbc78c42ffaa8b4412
Peter Drab <drabpe@sheridancollege.ca>
2021-06-04 18:05:04 -0400

    added forgotten imports and updated input once more had an extra > in a tag as well as added message.properties for error msg

--- src/main/java/sheridan/drabpe/rpsa1/controller/inputoutputController.java
+++ src/main/java/sheridan/drabpe/rpsa1/controller/inputoutputController.java
@@ -1,14 +1,18 @@
 package sheridan.drabpe.rpsa1.controller;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.servlet.ModelAndView;
+import sheridan.drabpe.rpsa1.domain.Choice;
+
 
 import java.awt.*;
 
+@Controller
 public class inputoutputController {
 
     @GetMapping(value ={"/", "/Input"})
