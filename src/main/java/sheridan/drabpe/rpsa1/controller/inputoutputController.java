03471071f8d08e93fbc77ddbd065e2cb4c219c91
Peter Drab <drabpe@sheridancollege.ca>
2021-06-04 17:33:33 -0400

    added to controller as well as input html

--- src/main/java/sheridan/drabpe/rpsa1/controller/inputoutputController.java
+++ src/main/java/sheridan/drabpe/rpsa1/controller/inputoutputController.java
@@ -1,4 +1,32 @@
 package sheridan.drabpe.rpsa1.controller;
 
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.annotation.Validated;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.awt.*;
+
 public class inputoutputController {
+
+    @GetMapping(value ={"/", "/Input"})
+    public ModelAndView input(){
+
+        return new ModelAndView("Input", "employee", new Choice());
+    }
+
+    @GetMapping("/Output")
+    public String output(@Validated @ModelAttribute Choice employee,
+                         BindingResult bindingResult,
+                         Model model){
+
+        if(bindingResult.hasErrors()){
+            return "Input";
+        }
+
+        model.addAttribute("employee", employee);
+        return "Output";
+    }
 }
