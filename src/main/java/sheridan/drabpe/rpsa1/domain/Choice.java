792a369951bc55765fa75af8815b1a874b970e3a
Peter Drab <drabpe@sheridancollege.ca>
2021-06-04 17:59:38 -0400

    updated choice class and input/output html

---
+++ src/main/java/sheridan/drabpe/rpsa1/domain/Choice.java
@@ -0,0 +1,34 @@
+package sheridan.drabpe.rpsa1.domain;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+import java.io.Serializable;
+
+public class Choice implements Serializable {
+
+    @Min(0)
+    @Max(2)
+    private int pick= 0;
+
+    public Choice(){
+    }
+
+    public Choice(int pick) {
+        this.pick = pick;
+    }
+
+    public int getPick() {
+        return pick;
+    }
+
+    public void setPick(int pick) {
+        this.pick = pick;
+    }
+
+    @Override
+    public String toString() {
+        return "Choice{" +
+                "pick=" + pick +
+                '}';
+    }
+}
