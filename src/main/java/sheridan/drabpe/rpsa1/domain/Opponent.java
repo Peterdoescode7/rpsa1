7c027c8cf60b5c27e0f123946c4738a3de9478db
Peter Drab <drabpe@sheridancollege.ca>
2021-06-04 19:33:26 -0400

    update to opponent classes

--- src/main/java/sheridan/drabpe/rpsa1/domain/Opponent.java
+++ src/main/java/sheridan/drabpe/rpsa1/domain/Opponent.java
@@ -1,11 +1,19 @@
 package sheridan.drabpe.rpsa1.domain;
 
+import org.springframework.stereotype.Controller;
+
+import javax.validation.constraints.Max;
+import javax.validation.constraints.Min;
+
 public class Opponent {
 
     private int oppPick= 0;
 
+    @Min(0)
+    @Max(2)
     public Opponent(){
     }
+
 
 
 }
