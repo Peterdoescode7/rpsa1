ec3b986433ea827b073b6922e6ffb2d250ba2153
Peter Drab <drabpe@sheridancollege.ca>
2021-06-04 21:28:32 -0400

    update to winner logic using case

--- src/main/java/sheridan/drabpe/rpsa1/domain/Winner.java
+++ src/main/java/sheridan/drabpe/rpsa1/domain/Winner.java
@@ -1,5 +1,9 @@
 package sheridan.drabpe.rpsa1.domain;
 
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 import java.io.Serializable;
 
 @SuppressWarnings("unused")
@@ -29,7 +33,58 @@ public class Winner implements Serializable {
                 '}';
     }
 
-    public String whoWins(int choice.pick, int opponent.oppPick){
-        return "mango";
+    @RequestMapping(value = "/Output", method = RequestMethod.POST)
+    public String whoWins(@RequestBody Choice choice, Opponent opponent){
+        String theWinner = "";
+        int userPick1 = choice.getPick();
+        int oppPick1 = opponent.getOppPick();
+        switch(userPick1){
+            case 0:
+                if(oppPick1 == 0) {
+                    theWinner = "Rock versus Rock, it's a tie!";
+                    return theWinner;
+                }
+                else if(oppPick1 == 1) {
+                    theWinner = "Rock versus Paper you lose!";
+                    return theWinner;
+                }
+                else if(oppPick1 == 2){
+                    theWinner = "Rock versus Scissors you win!";
+                    return theWinner;
+                }
+                break;
+            case 1:
+                if(oppPick1 == 0) {
+                    theWinner = "Paper versus Rock you win!";
+                    return theWinner;
+                }
+                else if(oppPick1 == 1) {
+                    theWinner = "Paper versus Paper, it's a tie!";
+                    return theWinner;
+                }
+                else if(oppPick1 == 2){
+                    theWinner = "Paper versus Scissors you lose!";
+                    return theWinner;
+                }
+                break;
+            case 2:
+                if(oppPick1 == 0) {
+                    theWinner = "Scissors versus Rock you lose!";
+                    return theWinner;
+                }
+                else if(oppPick1 == 1) {
+                    theWinner = "Scissors versus Paper you win!";
+                    return theWinner;
+                }
+                else if(oppPick1 == 2){
+                    theWinner = "Scissors versus Scissors, it's a tie!";
+                    return theWinner;
+                }
+                break;
+            }
+
+
+
+        return theWinner;
     }
 }
